ee121d38fe11cf373c72fdb48f5aedf32a12c81e
ericksebastian2003 <151951589+ericksebastian2003@users.noreply.github.com>
2024-11-15 15:04:06 -0500

    Aplicación Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"b-470cb","appId":"1:901764449978:web:e69f6f8a6f900815cfe483","storageBucket":"b-470cb.firebasestorage.app","apiKey":"AIzaSyAbMFYNMV63u8MY_EB9cWnjeN495wAkOz8","authDomain":"b-470cb.firebaseapp.com","messagingSenderId":"901764449978","measurementId":"G-JTVSPGD292"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideStorage(() => getStorage())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
